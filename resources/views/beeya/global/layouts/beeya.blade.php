9491640a7acbcae43601af1da385c84076120d3f
Angel Gonzalez <angel@capeandbay.com>
2019-09-03 22:58:03 -0400

    favicon...probably

--- resources/views/beeya/global/layouts/beeya.blade.php
+++ resources/views/beeya/global/layouts/beeya.blade.php
@@ -3,16 +3,16 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- <meta property="og:title" content="Beeya">
+    <meta property="og:title" content="Beeya">
     <meta property="og:description" content="Beeya">
-    <meta property="og:image" content="{!! env('APP_URL') !!}/img/Hero-BG.jpg">
-    <meta property="og:url" content="{!! Request::url() !!}"> -->
+    <meta property="og:image" content="{!! env('APP_URL') !!}/img/favicon.png">
+    <meta property="og:url" content="{!! Request::url() !!}">
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
     @csrf
 
     <title>{!! env('APP_NAME') !!}</title>
-    <!-- <link rel="shortcut icon" type="image/png" href="/img/the-blue-faviconz.png"/> -->
+    <link rel="shortcut icon" type="image/png" href="/img/favicon.png"/>
 
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i&display=swap" rel="stylesheet">
