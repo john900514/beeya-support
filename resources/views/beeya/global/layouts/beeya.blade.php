f6f708980f589752263b80554faa600484e5652a
Angel Gonzalez <angel@capeandbay.com>
2019-09-09 15:14:52 -0400

    AdRoll tracking Pixel

--- resources/views/beeya/global/layouts/beeya.blade.php
+++ resources/views/beeya/global/layouts/beeya.blade.php
@@ -99,6 +99,27 @@
                 a.appendChild(r);
             })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
         </script>
+
+        <script type="text/javascript">
+            adroll_adv_id = "TPKESJXCX5HDPCEFV53NSY";
+            adroll_pix_id = "FWI2EDL4HJFWFJTZP76UHU";
+
+            (function () {
+                var _onload = function(){
+                    if (document.readyState && !/loaded|complete/.test(document.readyState)){setTimeout(_onload, 10);return}
+                    if (!window.__adroll_loaded){__adroll_loaded=true;setTimeout(_onload, 50);return}
+                    var scr = document.createElement("script");
+                    var host = (("https:" == document.location.protocol) ? "https://s.adroll.com" : "http://a.adroll.com");
+                    scr.setAttribute('async', 'true');
+                    scr.type = "text/javascript";
+                    scr.src = host + "/j/roundtrip.js";
+                    ((document.getElementsByTagName('head') || [null])[0] ||
+                        document.getElementsByTagName('script')[0].parentNode).appendChild(scr);
+                };
+                if (window.addEventListener) {window.addEventListener('load', _onload, false);}
+                else {window.attachEvent('onload', _onload)}
+            }());
+        </script>
     @endif
     <!-- end pixels -->
 
